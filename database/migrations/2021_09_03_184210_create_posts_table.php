5ea02b868494cca770f64e65b19e5f26c7d44cfc
takiterina27th <nozomeel5296@yahoo.co.jp>
2021-09-03 18:46:59 +0900

    不要な記述を削除

--- database/migrations/2021_09_03_184210_create_posts_table.php
+++ database/migrations/2021_09_03_184210_create_posts_table.php
@@ -6,11 +6,6 @@ use Illuminate\Support\Facades\Schema;
 
 class CreatePostsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
